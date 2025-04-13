50c0d699cead1b0cbcb5ca2a7219315c7ac5dd72
Pranav Vashi <neobuddy89@gmail.com>
2025-04-13 23:26:16 +0800

    sm8550-common: Enable UDFPS animations

--- common.mk
+++ common.mk
@@ -143,6 +143,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.samsung
 
+TARGET_HAS_UDFPS := true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
